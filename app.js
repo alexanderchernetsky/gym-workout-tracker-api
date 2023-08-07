9c44aa1ca31e29d6508dfd90cdcfdd65def7437c
alex <a.chernetsky@itransition.com>
2023-08-07 12:36:51 +0100

    Change Access-Control-Allow-Headers value

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Origin, Content-Type, X-Auth-Token');
 
     // Pass to next layer of middleware
     next();
